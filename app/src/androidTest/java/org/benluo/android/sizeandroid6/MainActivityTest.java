c2bde9d4960c487ed352df10e033d9aeaf2ba0a8
Ben Luo <benluo@gmail.com>
2017-01-04 23:46:40 +0800

    start prepare tdd

---
+++ app/src/androidTest/java/org/benluo/android/sizeandroid6/MainActivityTest.java
@@ -0,0 +1,16 @@
+package org.benluo.android.sizeandroid6;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by benluo on 04/01/2017.
+ */
+public class MainActivityTest {
+    @Test
+    public void onCreate() throws Exception {
+
+    }
+
+}
